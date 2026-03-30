a1d5cf6a50f789731ff14e147771a637d4033060
Apurva Anand <apurvaanand51@gmail.com>
2026-03-30 11:11:57 +0530

    project-start

---
+++ martingale-strategy-simulation/src/scripts/martingale.js
@@ -0,0 +1,45 @@
+export function runMartingale(initialBalance, baseBet, totalRounds){
+    let balance = initialBalance;
+    let currentBet = baseBet;
+
+    let history = [];
+    let betHistory = [];
+    let resultHistory = [];
+
+    let isBankrupt = false;
+
+    for(let i = 1; i <= totalRounds; i++){
+
+        betHistory.push(currentBet);
+
+        let rand = Math.random();
+
+        if(rand < 0.5){
+            balance += currentBet;
+            resultHistory.push("W");
+
+            currentBet = baseBet;
+
+        } else {
+            balance -= currentBet;
+            resultHistory.push("L");
+
+            currentBet = currentBet * 2;
+        }
+
+        history.push(balance);
+
+        if(currentBet > balance){
+            isBankrupt = true;
+            break;
+        }
+    }
+
+    return {
+        history,
+        betHistory,
+        resultHistory,
+        finalBalance: balance,
+        isBankrupt
+    };
+}
