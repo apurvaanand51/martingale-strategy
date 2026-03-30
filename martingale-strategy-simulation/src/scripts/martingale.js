029b79a6e762cd792205cf8d36ae56b70e3f3b26
Apurva Anand <apurvaanand51@gmail.com>
2026-03-30 11:22:31 +0530

    running multiple martingale experients

--- martingale-strategy-simulation/src/scripts/martingale.js
+++ martingale-strategy-simulation/src/scripts/martingale.js
@@ -45,3 +45,55 @@ export function runMartingale(initialBalance, baseBet, totalRounds){
 }
 
 
+export function runMultipleMartingale(initialBalance, baseBet, totalRounds, totalExperiments){
+    let results = [];
+    let bankruptCount = 0;
+    let profitCount = 0;
+    let lossCount = 0;
+    let totalFinalBalance = 0;
+
+    let minBalance = Infinity;
+
+    for (let i = 1; i <= totalExperiments; i++){
+        let result = runMartingale(initialBalance, baseBet, totalRounds);
+
+        results.push(result.finalBalance);
+
+        if (result.isBankrupt) {
+            bankruptCount++;
+        }
+
+        if (result.finalBalance > initialBalance) {
+            profitCount++;
+        } else {
+            lossCount++;
+        }
+
+        if (result.finalBalance < minBalance) {
+            minBalance = result.finalBalance;
+        }
+
+        totalFinalBalance += result.finalBalance;
+    }
+
+    let avgBalance = totalFinalBalance / totalExperiments;
+
+    let bankruptcyRate = (bankruptCount / totalExperiments) * 100;
+    let profitRate = (profitCount / totalExperiments) * 100;
+
+    let netProfit = totalFinalBalance - (initialBalance * totalExperiments);
+    let avgProfit = netProfit / totalExperiments;
+
+    return {
+        results,
+        avgBalance,
+        bankruptcyRate,
+        profitRate,
+        netProfit,
+        avgProfit,
+        bankruptCount,
+        profitCount,
+        lossCount,
+        worstCase: minBalance
+    };
+}
