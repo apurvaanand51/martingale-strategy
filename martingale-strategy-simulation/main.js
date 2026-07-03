3e233f236498d3e6eb977a122ece6f1db23ed4e5
Apurva Anand <apurvaanand51@gmail.com>
2026-07-03 22:57:20 +0530

    source code button link fix

--- martingale-strategy-simulation/main.js
+++ martingale-strategy-simulation/main.js
@@ -4,6 +4,6 @@ btn.addEventListener("click", () => {
     window.location.reload();
 })
 
-document.querySelector(".sourcecode").addEventListener("click",() => {
+document.querySelector("#sourcecode").addEventListener("click",() => {
     window.location.assign("https://github.com/apurvaanand51/martingale-strategy");
 });
